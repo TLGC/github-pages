58cd67a8f69f766a43f546c14089fab7ce72cb2c
Mark Villar <markvillar24@protonmail.com>
2016-05-22 22:34:31 +0100

    Set LatLng to Basingstoke

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(51.28351, -1.08771), // Basingstoke
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
